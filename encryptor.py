dbe3b10e0e34e1c23181cdf78177a88d8c06b38f
FAnastasiaF <35655602+FAnastasiaF@users.noreply.github.com>
2020-04-17 14:58:42 +0300

    Update encryptor.py

--- encryptor.py
+++ encryptor.py
@@ -3,7 +3,8 @@ import argparse
 alphabet = 'abcdefghijklmnopqrstuvwxyz'
 alphabet2 = ''
 for i in range(16):
-    alphabet2 += chr(ord('a')+i)
+    alphabet2 += chr(ord('a') + i)
+
 
 class caesar:
 
@@ -13,9 +14,11 @@ class caesar:
             key = len(alphabet) + key
         for i in txt:
             if (i in alphabet):
-                output += alphabet[(alphabet.find(i) + key) % len(alphabet)]
-            elif(i.lower() in alphabet):
-                output += (alphabet[(alphabet.find(i.lower()) + key) % len(alphabet)]).upper()
+                n = (alphabet.find(i) + key) % len(alphabet)
+                output += alphabet[n]
+            elif (i.lower() in alphabet):
+                n = (alphabet.find(i.lower()) + key) % len(alphabet)
+                output += (alphabet[n]).upper()
             else:
                 output += i
         return output
@@ -25,9 +28,6 @@ class caesar:
         return self.encode(txt, key)
 
 
-alphabet = 'abcdefghijklmnopqrstuvwxyz'
-
-
 class vigenere:
 
     def encode(self, txt, key):
@@ -35,11 +35,15 @@ class vigenere:
         output = ''
         for i in txt:
             if (i in alphabet):
-                output += alphabet[(alphabet.find(i) + (alphabet.find(key[counter % len(key)]))) % len(alphabet)]
+                n = alphabet.find(i) + (alphabet.find(key[counter % len(key)]))
+                n %= len(alphabet)
+                output += alphabet[n]
                 counter += 1
             elif (i.lower() in alphabet):
-                output += (
-                alphabet[(alphabet.find(i.lower()) + (alphabet.find(key[counter % len(key)]))) % len(alphabet)]).upper()
+                n = alphabet.find(i.lower())
+                n += (alphabet.find(key[counter % len(key)]))
+                n %= len(alphabet)
+                output += (alphabet[n]).upper()
                 counter += 1
             else:
                 output += i
@@ -48,8 +52,11 @@ class vigenere:
     def decode(self, txt, key):
         key1 = ''
         for i in range(len(key)):
-            key1 += alphabet[(len(alphabet) - alphabet.find(key[i])) % len(alphabet)]
+            n = len(alphabet) - alphabet.find(key[i])
+            n %= len(alphabet)
+            key1 += alphabet[n]
         return self.encode(txt, key1)
+
 
 def train(txt):
     k = 0
@@ -62,11 +69,12 @@ def train(txt):
                 d[i.lower()] = 1
             k += 1
     for i in alphabet:
-        if(not (i in d)):
+        if (not (i in d)):
             d[i] = 0
     for j in d:
-        d[j] *= 100/k
+        d[j] *= 100 / k
     return d
+
 
 def hack(txt, model):
     txtanalysis = train(txt)
@@ -75,14 +83,15 @@ def hack(txt, model):
     minkey = 0
     for key in range(len(alphabet)):
         for i in range(len(alphabet)):
-            sum += ((txtanalysis[alphabet[(i + key) % len(alphabet)]] - model[alphabet[i]]))**2
-        if(minsum > sum):
+            n = txtanalysis[alphabet[(i + key) % len(alphabet)]]
+            n -= model[alphabet[i]]
+            sum += n ** 2
+        if (minsum > sum):
             minsum = sum
             minkey = key
         sum = 0
     obj = caesar()
-    return(obj.decode(txt, minkey))
-
+    return (obj.decode(txt, minkey))
 
 
 class vernam:
@@ -91,11 +100,15 @@ class vernam:
         output = ''
         counter = 0
         for i in txt:
-            if(i in alphabet2):
-                output += alphabet2[(alphabet2.find(key[counter]) ^ alphabet2.find(i)) % len(alphabet2)]
+            if (i in alphabet2):
+                n = alphabet2.find(key[counter]) ^ alphabet2.find(i)
+                n %= len(alphabet2)
+                output += alphabet2[n]
                 counter += 1
-            elif(i.lower() in alphabet2):
-                output += (alphabet2[(alphabet2.find(key[counter]) ^ alphabet2.find(i.lower())) % len(alphabet2)]).upper()
+            elif (i.lower() in alphabet2):
+                n = alphabet2.find(key[counter]) ^ alphabet2.find(i.lower())
+                n %= len(alphabet2)
+                output += (alphabet2[n]).upper()
                 counter += 1
             else:
                 output += i
@@ -104,16 +117,19 @@ class vernam:
     def decode(self, txt, key):
         return self.encode(txt, key)
 
+
 def input_file(file):
     text = (open(file, "r"))
     txt = text.read()
     text.close()
     return txt
 
+
 def output_file(file, output_txt):
     text = (open(file, "w"))
     txt = text.write(output_txt)
     text.close()
+
 
 def read_model(model):
     str = input_file(model)
@@ -122,120 +138,130 @@ def read_model(model):
     for j in range(len(str)):
         if (str[j] in alphabet):
             i = j
-            while(str[j+4] != ',' and str[j+4] != '}'):
-                number += str[j+4]
+            while (str[j + 4] != ',' and str[j + 4] != '}'):
+                number += str[j + 4]
                 j += 1
             model_dict[str[i]] = float(number)
             number = ""
     return model_dict
 
+
 def Encode(cipher, key, inputfile, outputfile):
-    if(inputfile!=None):
+    if (inputfile):
         try:
-           txt = input_file(inputfile)
+            txt = input_file(inputfile)
         except:
             print("I can't read file")
-            return
+            return None
     else:
         txt = input()
-    if(cipher == 'caesar'):
+    if (cipher == 'caesar'):
         try:
             caes = caesar()
             inputtxt = caes.encode(txt, int(key))
         except:
             print("Key must be integer")
-            return
-    elif(cipher == 'vigenere' or cipher == 'vernammod'):
+            return None
+    elif (cipher == 'vigenere' or cipher == 'vernammod'):
         for i in key:
-            if(not(i in alphabet)):
+            if (not (i in alphabet)):
                 print("Key must be lowercase")
-                return
-        if(cipher == 'vernammod'):
-            if(len(key)!=len(txt)):
+                return None
+        if (cipher == 'vernammod'):
+            if (len(key) != len(txt)):
                 print("Key must be same lenght with text")
+                return None
         vig = vigenere()
         inputtxt = vig.encode(txt, key)
-    elif(cipher == 'vernam'):
+    elif (cipher == 'vernam'):
         if (len(key) != len(txt)):
             print("Key must be same lenght with text")
+            return None
         for i in key:
-            if(not(i in alphabet)):
-                print("Key must consist of first 16 lowercase letters")
+            if (not (i in alphabet)):
+                print("Key must consist of first "
+                      "16 lowercase letters")
         ver = vernam()
         inputtxt = ver.decode(txt, key)
     else:
         print("Wrong cipher")
-        return
-    if (outputfile != None):
+        return None
+    if (outputfile):
         try:
             output_file(inputtxt, outputfile)
         except:
             print("I can't write in file")
-            return
+            return None
     else:
         print(inputtxt)
 
+
 def Decode(cipher, key, inputfile, outputfile):
-    if(inputfile!=None):
+    if (inputfile):
         try:
-           txt = input_file(inputfile)
+            txt = input_file(inputfile)
         except:
             print("I can't read file")
-            return
+            return None
     else:
         txt = input()
-    if(cipher == 'caesar'):
+    if (cipher == 'caesar'):
         try:
             int(key)
         except:
             print("Key must be integer")
-            return
+            return None
         caes = caesar()
         inputtxt = caes.decode(txt, int(key))
-    elif(cipher == 'vigenere' or cipher == 'vernammod'):
+    elif (cipher == 'vigenere' or cipher == 'vernammod'):
         for i in key:
-            if(not(i in alphabet)):
+            if (not (i in alphabet)):
                 print("Key must be lowercase")
-                return
-        if(cipher == 'vernammod'):
-            if(len(key)!=len(txt)):
+                return None
+        if (cipher == 'vernammod'):
+            if (len(key) != len(txt)):
                 print("Key must be same lenght with text")
+                return None
         vig = vigenere()
         inputtxt = vig.decode(txt, key)
-    elif(cipher == 'vernam'):
+    elif (cipher == 'vernam'):
         if (len(key) != len(txt)):
             print("Key must be same lenght with text")
+            return None
         for i in key:
-            if(not(i in alphabet)):
-                print("Key must consist of first 16 lowercase letters")
+            if (not (i in alphabet)):
+                print("Key must consist of first "
+                      "16 lowercase letters")
+                return None
         ver = vernam()
         inputtxt = ver.decode(txt, key)
     else:
         print("Wrong cipher")
-        return
-    if(outputfile != None):
+        return None
+    if (outputfile):
         try:
             output_file(inputtxt, outputfile)
         except:
             print("I can't write in file")
-            return
+            return None
     else:
         print(inputtxt)
 
+
 def Train(text, model):
-    if (text != None):
+    if (text):
         try:
             txt = input_file(text)
         except:
             print("I can't read file")
-            return
+            return None
     else:
         txt = input()
     try:
         output_file(model, str(train(txt)))
     except:
         print("Not way to output model file")
-        return
+        return None
 
 
 def Hack(inputfile, outputfile, modelfile):
@@ -243,60 +269,84 @@ def Hack(inputfile, outputfile, modelfile):
         model = read_model(modelfile)
     except:
         print("I can't read model file")
-        return
-    if (inputfile != None):
+        return None
+    if (inputfile):
         try:
             txt = input_file(inputfile)
         except:
             print("I can't read file")
-            return
+            return None
     else:
         txt = input()
     try:
         outputtxt = hack(txt, model)
     except:
         print("Problem with model")
-        return
-    if(outputfile == None):
-        print(outputtxt)
-    else:
+        return None
+    if (outputfile):
         try:
             output_file(outputfile, outputtxt)
         except:
             print("I can't write in file")
+            return None
+        print(outputtxt)
+    else:
+        print(outputtxt)
 
 
 def arguments():
     parser = argparse.ArgumentParser()
-    subparsers = parser.add_subparsers(dest='work', help='What does the program do')
+    subparsers = parser.add_subparsers(dest='work', help='What program do')
 
-    encode = subparsers.add_parser('encode', help='encode') #encode
-    encode.add_argument('--cipher', help='cipher - caesar/vigenere/vernammod/vernam')
-    encode.add_argument('--key', dest='key',help='key'
-                                                 '/integer for caesar'
-                                                 '/string lowercase letters for vigenere'
-                                                 '/file lenght string lowercase letters for vernammod'
-                                                 '/file lenght string with first 16 lowercase letters for vernam')
-    encode.add_argument('--input-file', dest='input', help='path to input file')
-    encode.add_argument('--output-file', dest='output', help='path to output file')
+    encode = subparsers.add_parser('encode', help='encode')  # encode
+    encode.add_argument('--cipher', help='cipher - caesar/'
+                                         'vigenere/'
+                                         'vernammod/'
+                                         'vernam')
+    encode.add_argument('--key', dest='key', help='key'
+                                                  '/integer for caesar'
+                                                  '/string lowercase letters'
+                                                  ' for vigenere'
+                                                  '/file lenght string '
+                                                  'lowercase letters '
+                                                  'for vernammod'
+                                                  '/file lenght string '
+                                                  'with first 16 lowercase'
+                                                  ' letters for vernam')
+    encode.add_argument('--input-file', dest='input', help='path '
+                                                           'to input file')
+    encode.add_argument('--output-file', dest='output', help='path '
+                                                             'to output file')
 
-    decode = subparsers.add_parser('decode', help='decode') #decode
-    decode.add_argument('--cipher', help='cipher - caesar/vigenere/vernammod/vernam')
+    decode = subparsers.add_parser('decode', help='decode')  # decode
+    decode.add_argument('--cipher', help='cipher - caesar/'
+                                         'vigenere/'
+                                         'vernammod/'
+                                         'vernam')
     decode.add_argument('--key', dest='key', help='key'
                                                   '/integer for caesar'
-                                                  '/string lowercase letters for vigenere'
-                                                  '/file lenght string lowercase letters for vernammod'
-                                                  '/file lenght string with first 16 lowercase letters for vernam')
-    decode.add_argument('--input-file', dest='input', help='path to input file')
-    decode.add_argument('--output-file', dest='output', help='path to output file')
+                                                  '/string lowercase letters'
+                                                  ' for vigenere'
+                                                  '/file lenght string '
+                                                  'lowercase letters '
+                                                  'for vernammod'
+                                                  '/file lenght string'
+                                                  ' with first 16 lowercase '
+                                                  'letters for vernam')
+    decode.add_argument('--input-file', dest='input', help='path to '
+                                                           'input file')
+    decode.add_argument('--output-file', dest='output', help='path to '
+                                                             'output file')
 
-    train = subparsers.add_parser('train', help='train') #train
-    train.add_argument('--text-file', dest='text', help='path to input text file')
+    train = subparsers.add_parser('train', help='train')  # train
+    train.add_argument('--text-file', dest='text', help='path '
+                                                        'to input text file')
     train.add_argument('--model-file', dest='model', help='output model file')
 
-    hack = subparsers.add_parser('hack', help='hack') #hack
+    hack = subparsers.add_parser('hack', help='hack')  # hack
     hack.add_argument('--input-file', dest='input', help='path to input file')
-    hack.add_argument('--output-file', dest='output', help='path to output file')
+    hack.add_argument('--output-file', dest='output', help='path to '
+                                                           'output file')
     hack.add_argument('--model-file', dest='model', help='input model file')
 
     args = parser.parse_args()
@@ -304,13 +354,11 @@ def arguments():
 
 
 args = arguments()
-if(args.work == 'encode'):
+if (args.work == 'encode'):
     Encode(args.cipher, args.key, args.input, args.output)
-elif(args.work == 'decode'):
+elif (args.work == 'decode'):
     Decode(args.cipher, args.key, args.input, args.output)
 elif (args.work == 'train'):
     Train(args.text, args.model)
 elif (args.work == 'hack'):
     Hack(args.input, args.output, args.model)
-
-
